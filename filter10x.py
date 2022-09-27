6b08a39beff7be1f116b96782cefc83da77fcac9
weide97 <wei-de-chang-1997@hotmail.com>
2022-09-27 23:57:54 +0800

    update

--- filter10x.py
+++ filter10x.py
@@ -15,6 +15,8 @@ def calculate_X(ticker):
         ticker_close =ticker_data["Close"]
         ticker__adj_close = ticker_data["Adj Close"]
         # if pd.to_datetime(ticker_close.keys()[0])==pd.to_datetime("2009-01-02"):
+        if ticker_close.head(1).values[0]> ticker_close.tail(1).values[0]:
+            return -1*round((ticker_close.tail(1).values / ticker_close.head(1).values)[0], 3)
         return round((ticker_close.tail(1).values / ticker_close.head(1).values)[0], 3)
     
     except:
@@ -25,7 +27,11 @@ def calculate_X_after(ticker):
         ticker_close =ticker_data["Close"]
         ticker__adj_close = ticker_data["Adj Close"]
         # if pd.to_datetime(ticker_close.keys()[0])==pd.to_datetime("2020-01-02"):
+        print((ticker_close.tail(1).values / ticker_close.head(1).values)[0], 3)
+        if ticker_close.head(1).values[0]> ticker_close.tail(1).values[0]:
+            return -1* round((ticker_close.tail(1).values / ticker_close.head(1).values)[0], 3)
         return round((ticker_close.tail(1).values / ticker_close.head(1).values)[0], 3)
+        
         # else:
         #     return 0
         # return round((ticker_close.tail(1).values / ticker_close.head(1).values)[0], 3)
@@ -37,12 +43,12 @@ def get_ticker_stats(ticker, start, end):
     ticker_adj_close = pdr.get_data_yahoo(ticker, start=start, end=end)["Adj Close"]
     ticker_return_ts = ticker_adj_close.pct_change().dropna()
     return pf.show_perf_stats(ticker_return_ts).Backtest.to_dict()
-
+# print(calculate_X_after("600201.SS"))
 # print(get_ticker_stats("AAPL", "2020-01-01", "2022-06-30"))
 
 # files = ["dataset\ChiNext Shares Only.CSV","dataset\Sci-Tech Innovation Board.CSV","dataset\SSE Mainboard Shares.CSV", "dataset\SZSE Mainboard Shares.CSV"]
-# files = ["dataset\ChiNext Shares Only.CSV","dataset\SZSE Mainboard Shares.CSV"]
-files = ["dataset\Sci-Tech Innovation Board.CSV","dataset\SSE Mainboard Shares.CSV"]
+files = ["dataset\ChiNext Shares Only.CSV","dataset\SZSE Mainboard Shares.CSV"]
+# files = ["dataset\Sci-Tech Innovation Board.CSV","dataset\SSE Mainboard Shares.CSV"]
 
 for file in files:
     print(file)
@@ -53,31 +59,37 @@ for file in files:
     beforeCovid = {}
     afterCovid  = {}
     for i in tickers:
-        print(i)
-        print(i.split(".")[0]+".SS")
-        number_x = calculate_X(i.split(".")[0]+".SS")
+        # print(i)
+        curr_ticker = i
+        # curr_ticker = i.split(".")[0]+".SS"
+        print(curr_ticker)
+        number_x = calculate_X(curr_ticker)
         # number_x = calculate_X(i)
 
         if number_x >= 10:
             beforeCovid[i] = number_x
-            number_x_after = calculate_X_after(i)
-            if number_x_after >= 10:
-                afterCovid[i] = number_x_after
+            number_x_after = calculate_X_after(curr_ticker)
+            print(i)
+            # if number_x_after >= 10:
+            afterCovid[i] = number_x_after
     print("==============================================================")
     print(beforeCovid)
     print("==============================================================")
 
+    print(afterCovid)
+    print("==============================================================")
+
     with open("ticker_check_before\\"+file_name, 'w', encoding='cp1252') as f:
         writer = csv.writer(f)
-        writer.writerow(["Symbols", "Value"])
+        writer.writerow(["Symbols", "BeforeCovid", "DuringCovid"])
         for key, value in beforeCovid.items():
-            writer.writerow([key, value])
+            writer.writerow([key, value, afterCovid[key] ])
         
-    with open("ticker_check_after\\"+file_name, 'w', encoding='cp1252') as f:
-        writer = csv.writer(f)
-        writer.writerow(["Symbols", "Value"])
-        for key, value in afterCovid.items():
-            writer.writerow([key, value])
+    # with open("ticker_check_after\\"+file_name, 'w', encoding='cp1252') as f:
+    #     writer = csv.writer(f)
+    #     writer.writerow(["Symbols", "Value"])
+    #     for key, value in afterCovid.items():
+    #         writer.writerow([key, value])
 
 # print(calculate_X("301349.SZ"))
 # df = pd.read_csv('dataset\Sci-Tech Innovation Board.csv', encoding='cp1252',  on_bad_lines='skip')
